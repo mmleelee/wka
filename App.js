98b3fec1a895e44920bfb66cdb21c06d2afbed56
Lee <s110919016@stu.ntue.edu.tw>
2022-02-22 22:16:18 +0800

    color

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#fae1dd',
     alignItems: 'center',
     justifyContent: 'center',
   },
