783b6768b7d9a571328b13be102da2a7cceb7d0b
Lee <s110919016@stu.ntue.edu.tw>
2022-02-22 22:19:07 +0800

    version2

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>數位二甲</Text>
+      <Text>110919016</Text>
       <StatusBar style="auto" />
     </View>
   );
